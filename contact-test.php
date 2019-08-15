bb1d2dc5fb572c7168c6fb52c12126dc0482089e
=Sasha Kovaliov <=sasha@justmeditate.com>
2019-08-15 13:49:20 -0700

    test form

---
+++ contact-test.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'nlupus@gmail.com';
+$recipient = 'mail@sashakovaliov.com';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
